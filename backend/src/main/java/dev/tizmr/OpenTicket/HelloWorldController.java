f842673237a9ebfb4cb280a78eb8415a7e5c8dda
TizianMr <tizianmr@gmail.com>
2026-04-03 15:59:15 +0200

    chore: initialized spring boot backend

---
+++ backend/src/main/java/dev/tizmr/OpenTicket/HelloWorldController.java
@@ -0,0 +1,13 @@
+package dev.tizmr.OpenTicket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @GetMapping(path = "/hello")
+  public String helloWorld() {
+    return "Hello World";
+  }
+}
