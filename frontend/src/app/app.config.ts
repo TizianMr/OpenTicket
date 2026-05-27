71e985d7ece047e8d35c7856f2529262bc662e2f
TizianMr <tizianmr@gmail.com>
2026-05-27 20:19:20 +0200

    feat: resolved frontend linting and code quality issues

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,14 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([baseUrlInterceptor]))
-  ]
+    provideHttpClient(withInterceptors([baseUrlInterceptor])),
+  ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { Dashboard } from './features/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Dashboard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-}
+export class App {}
