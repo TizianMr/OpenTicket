7c8943d5a295b33e2c4b78e107ec53e96bc73de9
TizianMr <tizianmr@gmail.com>
2026-05-31 12:22:50 +0200

    feat: added OpenAPI documentation

--- backend/src/main/java/dev/tizmr/OpenTicket/controllers/GlobalExceptionHandler.java
+++ backend/src/main/java/dev/tizmr/OpenTicket/controllers/GlobalExceptionHandler.java
@@ -5,13 +5,15 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ErrorDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
 
     final String errorMsg =
--- backend/src/main/java/dev/tizmr/OpenTicket/controllers/TicketController.java
+++ backend/src/main/java/dev/tizmr/OpenTicket/controllers/TicketController.java
@@ -1,10 +1,16 @@
 package dev.tizmr.OpenTicket.controllers;
 
 import dev.tizmr.OpenTicket.domain.dto.CreateTicketRequestDto;
+import dev.tizmr.OpenTicket.domain.dto.ErrorDto;
 import dev.tizmr.OpenTicket.domain.dto.TicketDto;
 import dev.tizmr.OpenTicket.domain.entity.Ticket;
 import dev.tizmr.OpenTicket.mapper.TicketMapper;
 import dev.tizmr.OpenTicket.service.TicketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +28,17 @@ public class TicketController {
     this.ticketMapper = ticketMapper;
   }
 
+  @Operation(summary = "Create a ticket")
+  @ApiResponses({
+    @ApiResponse(responseCode = "201", description = "User created"),
+    @ApiResponse(
+        responseCode = "400",
+        description = "Validation failed",
+        content =
+            @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorDto.class)))
+  })
   @PostMapping
   public ResponseEntity<TicketDto> createTicket(
       @Valid @RequestBody CreateTicketRequestDto createTicketRequestDto) {
