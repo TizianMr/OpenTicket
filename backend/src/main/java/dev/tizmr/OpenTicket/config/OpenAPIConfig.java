7d3372d5c3ea1cec2760c4e4e1b788ab45db3148
TizianMr <tizianmr@gmail.com>
2026-05-31 14:32:20 +0200

    feat: mark properties in open api spec as non nullable by default

--- backend/src/main/java/dev/tizmr/OpenTicket/config/OpenAPIConfig.java
+++ backend/src/main/java/dev/tizmr/OpenTicket/config/OpenAPIConfig.java
@@ -2,8 +2,13 @@ package dev.tizmr.OpenTicket.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class OpenAPIConfig {
@@ -11,5 +16,23 @@ public class OpenAPIConfig {
   @Bean
   public OpenAPI api() {
     return new OpenAPI().info(new Info().title("OpenTicket API").version("1.0.0"));
+  }
+
+  @Bean
+  public OpenApiCustomizer requiredFieldsCustomizer() {
+    return openApi -> openApi.getComponents()
+      .getSchemas()
+      .values()
+      .forEach(rawSchema -> {
+        Schema<?> schema = (Schema<?>) rawSchema;
+        Map<?, ?> rawProperties = schema.getProperties();
+        if (rawProperties != null) {
+          List<String> required = rawProperties.entrySet().stream()
+            .filter(e -> !Boolean.TRUE.equals(((Schema<?>) e.getValue()).getNullable()))
+            .map(e -> (String) e.getKey())
+            .toList();
+          schema.setRequired(required);
+        }
+      });
   }
 }
