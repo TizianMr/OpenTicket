2d119043c60494461da4c598061616e1cd759d3d
TizianMr <tizianmr@gmail.com>
2026-06-05 10:46:34 +0200

    refactor: moved pagination into own component

---
+++ frontend/src/app/features/ticket/ticket-table/ticket-table-pagination/ticket-table-pagination.ts
@@ -0,0 +1,44 @@
+import { Component, computed, input, output } from '@angular/core';
+
+import { ChevronLeft } from '../../../../common/icons/chevron-left';
+import { ChevronLeftDouble } from '../../../../common/icons/chevron-left-double';
+import { ChevronRight } from '../../../../common/icons/chevron-right';
+import { ChevronRightDouble } from '../../../../common/icons/chevron-right-double';
+import { PagingResultTicketDto } from '../../../../core/api-generated';
+
+@Component({
+  selector: 'app-ticket-table-pagination',
+  imports: [ChevronLeftDouble, ChevronLeft, ChevronRight, ChevronRightDouble],
+  templateUrl: './ticket-table-pagination.html',
+})
+export class TicketTablePagination {
+  paginationState = input.required<Omit<PagingResultTicketDto, 'content'>>();
+  pageChange = output<number>();
+
+  readonly isLastPage = computed(() => this.paginationState().page === this.paginationState().totalPages - 1);
+  readonly isFirstPage = computed(() => this.paginationState().page === 0);
+  readonly tableInfo = computed(() => ({
+    min: this.paginationState().totalElements === 0 ? 0 : this.paginationState().page * this.paginationState().size + 1,
+    max: Math.min(
+      this.paginationState().totalElements,
+      (this.paginationState().page + 1) * this.paginationState().size,
+    ),
+    total: this.paginationState().totalElements,
+  }));
+
+  onForwardClick(): void {
+    this.pageChange.emit(this.paginationState().page + 1);
+  }
+
+  onBackwardsClick(): void {
+    this.pageChange.emit(this.paginationState().page - 1);
+  }
+
+  onFirstPageClick(): void {
+    this.pageChange.emit(0);
+  }
+
+  onLastPageClick(): void {
+    this.pageChange.emit(this.paginationState().totalPages - 1);
+  }
+}
--- frontend/src/app/features/ticket/ticket-table/ticket-table.ts
+++ frontend/src/app/features/ticket/ticket-table/ticket-table.ts
@@ -1,16 +1,13 @@
 import { DatePipe } from '@angular/common';
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
-import { ChevronLeft } from '../../../common/icons/chevron-left';
-import { ChevronLeftDouble } from '../../../common/icons/chevron-left-double';
-import { ChevronRight } from '../../../common/icons/chevron-right';
-import { ChevronRightDouble } from '../../../common/icons/chevron-right-double';
+import { TicketTablePagination } from './ticket-table-pagination/ticket-table-pagination';
 import { PagingResultTicketDto, TicketsService } from '../../../core/api-generated';
 import { CreateTicket } from '../create-ticket/create-ticket';
 
 @Component({
   selector: 'app-ticket-table',
-  imports: [DatePipe, CreateTicket, ChevronLeft, ChevronRight, ChevronRightDouble, ChevronLeftDouble],
+  imports: [DatePipe, CreateTicket, TicketTablePagination],
   templateUrl: './ticket-table.html',
   styleUrl: './ticket-table.css',
 })
@@ -37,28 +34,9 @@ export class TicketTable implements OnInit {
     size: 5,
     page: 0,
   });
-  readonly isLastPage = computed(() => this.tableState().page === this.tableState().totalPages - 1);
-  readonly isFirstPage = computed(() => this.tableState().page === 0);
-  readonly tableInfo = computed(() => ({
-    min: this.tableState().totalElements === 0 ? 0 : this.tableState().page * this.tableState().size + 1,
-    max: Math.min(this.tableState().totalElements, (this.tableState().page + 1) * this.tableState().size),
-    total: this.tableState().totalElements,
-  }));
 
-  onPageForwardClick(): void {
-    this.loadTickets(this.tableState().page + 1);
-  }
-
-  onPageBackwardsClick(): void {
-    this.loadTickets(this.tableState().page - 1);
-  }
-
-  onFirstPageClick(): void {
-    this.loadTickets(0);
-  }
-
-  onLastPageClick(): void {
-    this.loadTickets(this.tableState().totalPages - 1);
+  onPageChange(newPage: number): void {
+    this.loadTickets(newPage);
   }
 
   ngOnInit(): void {
