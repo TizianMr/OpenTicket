e8e8c655df047b0b44d47dfcca81d2e73d093f26
TizianMr <tizianmr@gmail.com>
2026-06-06 18:50:53 +0200

    refactor: rename directive folder

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 
 import { DocumentPlus } from '../../common/icons/document-plus';
-import { ModalDirective } from '../../core/directive/modal-directive';
+import { ModalDirective } from '../../core/directives/modal-directive';
 import { ModalService } from '../../core/services/modal-service';
 import { CreateTicket } from '../ticket/create-ticket/create-ticket';
 import { TicketTable } from '../ticket/ticket-table/ticket-table';
