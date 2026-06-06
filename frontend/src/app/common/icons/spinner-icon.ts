4e6a4597ab6d47bb9f16d0f73d102a77df376f5a
TizianMr <tizianmr@gmail.com>
2026-06-06 13:30:23 +0200

    feat: implemented table loading state

--- frontend/src/app/common/icons/spinner-icon.ts
+++ frontend/src/app/common/icons/spinner-icon.ts
@@ -18,12 +18,13 @@ import { Component, input } from '@angular/core';
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
   `,
   host: {
-    'class': 'animate-spin size-5',
-    '[attr.viewBox]': 'viewBox()',
+    'class': 'animate-spin',
+    '[class]': '"size-" + size()',
+    '[attr.viewBox]': '"0 0 24 24"',
     '[attr.xmlns]': '"http://www.w3.org/2000/svg"',
     '[attr.fill]': '"none"',
   },
 })
 export class SpinnerIcon {
-  readonly viewBox = input<string>('0 0 24 24');
+  readonly size = input<string>('5');
 }
--- frontend/src/app/features/ticket/ticket-table/ticket-table.ts
+++ frontend/src/app/features/ticket/ticket-table/ticket-table.ts
@@ -1,9 +1,12 @@
 import { DatePipe } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 
 import { TicketTablePagination } from './ticket-table-pagination/ticket-table-pagination';
 import { PagingResultTicketDto, TicketsService } from '../../../core/api-generated';
 import { ModalService } from '../../../core/services/modal-service';
+
+// TODO: move?
+const PAGE_SIZE = 25;
 
 @Component({
   selector: 'app-ticket-table',
@@ -16,16 +19,12 @@ export class TicketTable implements OnInit {
   private modalService = inject(ModalService);
 
   hasError = signal(false);
+  isLoading = signal(false);
 
   protected readonly headers = ['Title', 'Status', 'Created at', 'Updated at'];
 
-  readonly tableState = signal<PagingResultTicketDto>({
-    content: [],
-    totalPages: 0,
-    totalElements: 0,
-    size: 5,
-    page: 0,
-  });
+  readonly tableState = signal<PagingResultTicketDto | undefined>(undefined);
+  isEmpty = computed(() => this.tableState()?.totalElements === 0);
 
   onPageChange(newPage: number): void {
     this.loadTickets(newPage);
@@ -40,16 +39,27 @@ export class TicketTable implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loadTickets(this.tableState().page);
+    this.loadTickets(0);
   }
 
   private loadTickets(page: PagingResultTicketDto['page']): void {
     this.hasError.set(false);
-    this.ticketService.listTickets(page, this.tableState().size).subscribe({
+
+    const spinnerTimer = setTimeout(() => this.isLoading.set(true), 500);
+
+    this.ticketService.listTickets(page, PAGE_SIZE).subscribe({
       next: response => {
         this.tableState.set(response);
       },
-      error: () => this.hasError.set(true),
+      error: () => {
+        clearTimeout(spinnerTimer);
+        this.isLoading.set(false);
+        this.hasError.set(true);
+      },
+      complete: () => {
+        clearTimeout(spinnerTimer);
+        this.isLoading.set(false);
+      },
     });
   }
 }
