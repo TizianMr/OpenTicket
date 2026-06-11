619fc1672f58ca93f1a927c73805ea5f5782dca6
TizianMr <tizianmr@gmail.com>
2026-06-11 19:56:08 +0200

    feat: display sorting icons for table headers

---
+++ frontend/src/app/common/icons/chevron-up.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @angular-eslint/component-selector */
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'svg[chevron-up]',
+  template: `
+    <svg:path
+      d="m5.84 15.41l5.66-5.66l5.66 5.66l-.71.7l-4.95-4.95l-4.95 4.95z"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      fill="none" />
+  `,
+  host: {
+    'class': 'size-3',
+    '[attr.viewBox]': 'viewBox()',
+    '[attr.xmlns]': '"http://www.w3.org/2000/svg"',
+    'aria-hidden': 'true',
+    'role': 'img',
+  },
+})
+export class ChevronUp {
+  readonly viewBox = input<string>('0 0 24 24');
+}
--- frontend/src/app/features/ticket/ticket-table/ticket-table-header/ticket-table-header.ts
+++ frontend/src/app/features/ticket/ticket-table/ticket-table-header/ticket-table-header.ts
@@ -1,8 +1,12 @@
 import { Component, input } from '@angular/core';
 
+import { ChevronDown } from '../../../../common/icons/chevron-down';
+import { ChevronUp } from '../../../../common/icons/chevron-up';
+
 @Component({
   selector: 'app-ticket-table-header',
   templateUrl: './ticket-table-header.html',
+  imports: [ChevronDown, ChevronUp],
 })
 export class TicketTableHeader {
   readonly headers = input.required<string[]>();
