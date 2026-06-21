b4f526d5b25cc1d716e536eaaf73f4d945c91c5d
TizianMr <tizianmr@gmail.com>
2026-06-21 13:23:27 +0200

    test: updated ticket table header tests

--- frontend/src/app/features/ticket/ticket-table/ticket-table-header/ticket-table-header.spec.ts
+++ frontend/src/app/features/ticket/ticket-table/ticket-table-header/ticket-table-header.spec.ts
@@ -13,7 +13,11 @@ describe('TicketTableHeader', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(TicketTableHeader);
-    fixture.componentRef.setInput('headers', '[A, B, C]');
+    fixture.componentRef.setInput('headers', [
+      { key: 'name', label: 'Name', sortDirection: undefined },
+      { key: 'age', label: 'Age', sortDirection: SortDirection.DESC },
+      { key: 'email', label: 'Email', sortDirection: SortDirection.ASC },
+    ]);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
@@ -22,11 +26,27 @@ describe('TicketTableHeader', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should emit sort event with correct params', () => {
+  it('should emit sort event ASC', () => {
     const emitSpy = vi.spyOn(component.sort, 'emit');
 
-    component.onSort('A', SortDirection.ASC);
+    component.onSort('name');
 
-    expect(emitSpy).toHaveBeenCalledWith({ key: 'A', direction: SortDirection.ASC });
+    expect(emitSpy).toHaveBeenCalledWith({ key: 'name', direction: SortDirection.ASC });
+  });
+
+  it('should emit sort event DESC', () => {
+    const emitSpy = vi.spyOn(component.sort, 'emit');
+
+    component.onSort('email');
+
+    expect(emitSpy).toHaveBeenCalledWith({ key: 'email', direction: SortDirection.DESC });
+  });
+
+  it('should emit sort event UNDEFINED', () => {
+    const emitSpy = vi.spyOn(component.sort, 'emit');
+
+    component.onSort('age');
+
+    expect(emitSpy).toHaveBeenCalledWith({ key: 'age', direction: undefined });
   });
 });
